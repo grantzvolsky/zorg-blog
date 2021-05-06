5d70ec0ac1bad9f45a3499dcd4e4da87d810e877
Grant Zvolsky <grant@zvolsky.org>
2021-05-06 10:10:24 +0200

    add routing

--- spa/src/index.tsx
+++ spa/src/index.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Link, navigate, RouteComponentProps, Router, useLocation } from "@reach/router"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const header = <React.Fragment><h1><Link to='/'>zvolsky.org</Link> ~ Grant Zvolsky</h1></React.Fragment>
 
-reportWebVitals(console.log);
+function IndexRoute(props: RouteComponentProps) {
+  return <React.Fragment>
+    {header}
+    <ul>
+      <li><Link to='/hello-world'>Hello World</Link></li>
+    </ul>
+
+  </React.Fragment>
+}
+
+function ArticleRoute(props: RouteComponentProps) {
+  return <React.Fragment>
+    {header}
+    <h2>{props.path}</h2>
+  </React.Fragment>
+}
+
+const app = <React.StrictMode><Router>
+  <IndexRoute path='/' />
+  <ArticleRoute path='/hello-world' />
+</Router></React.StrictMode>
+
+ReactDOM.render(app, document.getElementById('root'));
